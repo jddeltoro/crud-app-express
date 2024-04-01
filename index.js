45fff671fcc4ddce7df501dfe0fa047e331ab233
jddeltoro <jdiazdeltoro@gmail.com>
2024-04-01 18:00:29 -0400

    support post products

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 const express  = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const { stringify } = require('nodemon/lib/utils');
+const Product = require('./models/product.models');
 
+
+app.use(express.json()); 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.get('/bye', (req, res) => {
-    res.send('Bye World');
+
+app.post('/api/products', async (req, res) => {
+    try {
+        const product = await Product.create({
+            name: req.body.name,
+            price: req.body.price,
+            quantity: req.body.quantity,
+            image: req.body.image
+        });
+        res.send(product);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(error.message);
+    }
 });
+
+mongoose.connect('mongodb+srv://jdiazdeltoro:H6AvoixQ04RVvDCq@crud-app.q0t14xf.mongodb.net/?retryWrites=true&w=majority&appName=crud-app', {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB', err);
+    });
 
 app.listen(3001, () => {
     console.log('Server is running on port 3001');
