dab96bc7826fad873eee54801eb2a499c4285cc2
jddeltoro <jdiazdeltoro@gmail.com>
2024-04-02 01:36:37 -0400

    support crud over products

--- index.js
+++ index.js
@@ -26,6 +26,62 @@ app.post('/api/products', async (req, res) => {
     }
 });
 
+app.get('/api/products', async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.status(200).send(products);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(error.message);
+    }
+}); 
+
+app.get('/api/product/:id', async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        if (!product) {
+            return res.status(404).send('Product not found');
+        }
+        res.status(200).send(product);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(error.message);
+    }
+});
+
+app.put('/api/product/:id', async (req, res) => {
+    try {
+        const product = await Product.findByIdAndUpdate(req.params.id, {
+            name: req.body?.name,
+            price: req.body?.price,
+            quantity: req.body?.quantity,
+            image: req.body?.image
+        }, { new: true });
+        if (!product) {
+            return res.status(404).send('Product not found');
+        }
+        res.status(200).send(product);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(error.message);
+    }
+});
+
+app.delete('/api/product/:id', async (req, res) => {
+    try {
+        const product = await Product.findByIdAndDelete(req.params.id);
+        if (!product) {
+            return res.status(404).send('Product not found');
+        }
+        res.status(200).send(product);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(error.message);
+    }
+});
+
+
+
 mongoose.connect('mongodb+srv://jdiazdeltoro:H6AvoixQ04RVvDCq@crud-app.q0t14xf.mongodb.net/?retryWrites=true&w=majority&appName=crud-app', {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => {
         console.log('Connected to MongoDB');
