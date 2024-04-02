7eef8820f6c28d1705989a1f1ffa5e17db125938
jddeltoro <jdiazdeltoro@gmail.com>
2024-04-02 01:59:37 -0400

    add product routes

--- index.js
+++ index.js
@@ -1,86 +1,20 @@
 const express  = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const { stringify } = require('nodemon/lib/utils');
-const Product = require('./models/product.models');
+const productRoute = require('./routes/product.route');
 
 
+//Middleware
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
+
+
+//Routes
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('CRUD App with Node.js and MongoDB');
 });
 
-
-app.post('/api/products', async (req, res) => {
-    try {
-        const product = await Product.create({
-            name: req.body.name,
-            price: req.body.price,
-            quantity: req.body.quantity,
-            image: req.body.image
-        });
-        res.send(product);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send(error.message);
-    }
-});
-
-app.get('/api/products', async (req, res) => {
-    try {
-        const products = await Product.find();
-        res.status(200).send(products);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send(error.message);
-    }
-}); 
-
-app.get('/api/product/:id', async (req, res) => {
-    try {
-        const product = await Product.findById(req.params.id);
-        if (!product) {
-            return res.status(404).send('Product not found');
-        }
-        res.status(200).send(product);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send(error.message);
-    }
-});
-
-app.put('/api/product/:id', async (req, res) => {
-    try {
-        const product = await Product.findByIdAndUpdate(req.params.id, {
-            name: req.body?.name,
-            price: req.body?.price,
-            quantity: req.body?.quantity,
-            image: req.body?.image
-        }, { new: true });
-        if (!product) {
-            return res.status(404).send('Product not found');
-        }
-        res.status(200).send(product);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send(error.message);
-    }
-});
-
-app.delete('/api/product/:id', async (req, res) => {
-    try {
-        const product = await Product.findByIdAndDelete(req.params.id);
-        if (!product) {
-            return res.status(404).send('Product not found');
-        }
-        res.status(200).send(product);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send(error.message);
-    }
-});
-
+app.use('/api/products', productRoute);
 
 
 mongoose.connect('mongodb+srv://jdiazdeltoro:H6AvoixQ04RVvDCq@crud-app.q0t14xf.mongodb.net/?retryWrites=true&w=majority&appName=crud-app', {useNewUrlParser: true, useUnifiedTopology: true})
---
+++ routes/product.route.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const productController = require('../controllers/product.controller');
+
+
+router.get('/', productController.getAllProducts);
+
+router.post('/', productController.createProduct);
+
+router.get('/:id', productController.getProductById);
+
+router.put('/:id', productController.updateProduct);
+
+router.delete('/:id', productController.deleteProduct);
+
+module.exports = router;
